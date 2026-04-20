8e57ca4bdb2d8123355f058cf492a573c63766e7
ladev325 <vlad2008artemov@gmail.com>
2026-04-20 20:08:50 +0300

    Commit

--- index.js
+++ index.js
@@ -1,10 +1,39 @@
-const express = require('express');
-const path = require('path');
+const { Telegraf } = require('telegraf')
+const { message } = require('telegraf/filters')
 
-const app = express()
+const BOT_TOKEN = "8559939276:AAGystwCez5v2-F5BLumbbVScl6hnyCqPfE"
+const bot = new Telegraf(BOT_TOKEN)
 
-app.use(express.static(path.join(__dirname, 'html')));
+let tmr = null;
 
-app.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000')
+bot.start((ctx) => {
+    ctx.reply('Timer! Usage: /tmr mm');
 })
+
+bot.on('text', async (ctx) => {
+    const userText = ctx.message.text;
+    const chatId = ctx.chat.id;
+
+    if (userText.includes("/tmr ")) {
+        const number = userText.replace("/tmr ", "");
+        if (!isNaN(parseInt(number))) {
+            ctx.reply(`Set timer for ${number} minutes!`);
+            setTimeout(() => {
+                bot.telegram.sendMessage(chatId, `TIMER FOR ${number} MINUTES IS OUT!!!!!!!!!!!!!!!!!!!!!`);
+            }, number * 60000 / 60) // seconds by now for testing
+        }
+        else {
+            ctx.reply("Wrong number. Usage: /tmr mm");
+        }
+    }
+    else {
+        ctx.reply("Wrong command. Usage: /tmr mm");
+    }
+})
+
+bot.launch({
+    dropPendingUpdates: true
+})
+
+process.once('SIGINT', () => bot.stop('SIGINT'))
+process.once('SIGTERM', () => bot.stop('SIGTERM'))
